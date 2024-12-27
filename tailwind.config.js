879b20640c2ff0c26bcbe6a7e7d295ae05595558
theofanihati <theofanihati@gmail.com>
2024-12-28 03:06:51 +0700

    slicing login & register. TODO make the cover screen height

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '.public/index.html',
+    './public/register.html',
+    './public/login.html',
   ],
   theme: {
     extend: {},
+    colors: {
+      customPink1: '#ffe9ec',
+      customPink2: '#f5b6be',
+      customPink3: '#de8bb0',
+      customPurple1: '#cea9c6',
+      customGray1: '#d9d9d9',
+      customGray2: '#a6a6a6',
+      customGray3: '#737373',
+      red: '#ff3131',
+      white: '#ffffff',
+      black: '#000000',
+    }
   },
   plugins: [],
 }
